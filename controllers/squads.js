eb16b1644dfff36bb1d9a415c33631b6358875f0
Colin MacDonald <colinmacdonald1213@yahoo.com>
2020-09-01 00:17:17 -0400

    more module exports

--- controllers/squads.js
+++ controllers/squads.js
@@ -4,7 +4,9 @@ module.exports = {
     show,
     new: newSquad,
     create,
-    edit
+    edit, 
+    update,
+    delete: deleteSquad
 }
 function index(req, res) {
     Squad.find({}, function(err, squads) {
@@ -40,4 +42,21 @@ function newSquad(req, res) {
         }
         res.render('squads/edit', { squad, title: 'Edit Squad'})
     })
+  }
+
+  function update(req, res) {
+    Squad.findByIdAndUpdate(req.params.id, req.body, {new: true}, function(err, squad) {
+        if (err) {
+            res.render('squads/edit', { squad, title: 'Edit Squad'})
+        }
+        res.redirect(`squads`)
+    })
+  }
+
+  function deleteSquad(req, res) {
+    Squad.findByIdAndDelete(req.params.id, function(err) {
+        Character.remove({squad: req.params.id}, function(err) {
+            res.redirect('/squads');
+        });
+    });
   }
