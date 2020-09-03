46869331fb14d241bee2210efa73e1a0a2000f88
Colin MacDonald <colinmacdonald1213@yahoo.com>
2020-09-03 18:51:37 -0400

    finalize

--- routes/squads.js
+++ routes/squads.js
@@ -5,10 +5,17 @@ const squadsCtrl = require('../controllers/squads')
 router.get('/', squadsCtrl.index);
 router.get('/new', squadsCtrl.new);
 router.get('/:id', squadsCtrl.show);
-router.post('/', squadsCtrl.create);
-router.delete('/:id', squadsCtrl.delete);
+router.post('/', isLoggedIn, squadsCtrl.create);
+router.delete('/:id', isLoggedIn, squadsCtrl.delete);
 router.get('/:id/edit', squadsCtrl.edit);
 router.put('/:id', squadsCtrl.update);
 router.post('/:id/characters', squadsCtrl.add)
+
+
+function isLoggedIn(req, res, next) {
+    if ( req.isAuthenticated() ) return next();
+    res.redirect('/auth/google');
+  }
+
 
 module.exports = router;
